9cca45c5a571f780a1c1127bb89d5db799c26b35
sal12321 <aaqibalam290@gmail.com>
2026-07-30 10:32:49 +0530

    Initial commit

---
+++ src/main/java/com/sal/Oauth2/controller/Home.java
@@ -0,0 +1,47 @@
+package com.sal.Oauth2.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class Home {
+
+    @GetMapping("/")
+    public String goHome(@AuthenticationPrincipal OAuth2User user, Model model, Authentication authentication){
+
+        user.getAttributes().forEach((k,v) ->
+                System.out.println(k + " : " + v));
+
+
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+
+        String provider = token.getAuthorizedClientRegistrationId().toUpperCase();
+        String email = user.getAttribute("email");
+        if(email == null){
+            email = "Not Provided !";
+        }
+
+        model.addAttribute("provider", provider);
+        model.addAttribute("name", user.getAttribute("name"));
+            model.addAttribute("email", email);
+        model.addAttribute("picture", user.getAttribute("picture"));
+
+
+        return "Home" ;
+    }
+//    @GetMapping("/fuck")
+//
+//    public String fuck(){
+//        return "fuck you" ;
+//    }
+}
---
+++ src/main/java/com/sal/Oauth2/security/SpringSecurity.java
@@ -0,0 +1,29 @@
+package com.sal.Oauth2.security;
+
+import org.apache.catalina.filters.HttpHeaderSecurityFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        
+        http.
+                authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+
+                .oauth2Login(oauth -> oauth.defaultSuccessUrl("/", true));
+
+        
+        
+        
+        return http.build();
+
+    }
+}
