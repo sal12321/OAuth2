c165b717a0db5fa5c46fcd51a2a2201bd72f9b66
sal12321 <aaqibalam290@gmail.com>
2026-07-30 13:52:18 +0530

    resoving the dependencies

--- src/main/java/com/sal/Oauth2/security/SpringSecurity.java
+++ src/main/java/com/sal/Oauth2/security/SpringSecurity.java
@@ -15,16 +15,16 @@ public class SpringSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-//            http.
-//                    authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-//
-//                    .oauth2Login(oauth -> oauth.defaultSuccessUrl("/", true));
+            http.
+                    authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                    .oauth2Login(oauth -> oauth.defaultSuccessUrl("/", true));
+
+//        http
+//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
-        
+
 
 
 
